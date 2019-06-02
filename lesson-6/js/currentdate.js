6b698404e1e0356e403b7e0f27df14f189bc75b4
Sara Figueiredo <saratf@gmail.com>
2019-06-02 00:47:16 -0600

    1st 6/2

--- lesson-6/js/currentdate.js
+++ lesson-6/js/currentdate.js
@@ -1,15 +1,15 @@
-function dateForFooter() {
 
+//declares a variable that is going to use the function new Date()
 var myDate = new Date();
 
+//declares the arrays with the names I want to display in the date format
 var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+//declares a variable that is going to receive all the functions necessary for the information I need with array positions
+var d = (dayOfWeek[myDate.getDay()] + "," + " " + myDate.getDate() + " " + monthName[myDate.getMonth()] + " " + myDate.getFullYear());;
 
 
-var FooterDate = [dayOfWeek.getDay()] + getDate() + [monthName.getMonth()] + myDate.getFullYear();
 
-document.getElementById('outputdiv').innerHTML = FooterDate;
-
-}
+document.getElementById("CalendarDate").innerHTML = d;
