d26ed176acce2d81964739f6b993ef287939be6d
Sara Figueiredo <saratf@gmail.com>
2019-06-02 02:14:08 -0600

    3rd 6/1/19

---
+++ lesson-6/js/hamburger.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+        document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    }
+
+
+/*
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("mystyle");
+}
+*/
